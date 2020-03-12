70bf23473842e3f252e1c03df4471f8c604b8b27
Preston Hudson <prestonleehudson@gmail.com>
2020-03-12 10:18:12 -0500

    Created a for loop to iterate i through 1 to 101. Prints Fizz and Buzz.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,2 +1,13 @@
 class FizzBuzz:
-    
+
+    def fizz_buzz():
+
+        for i in range(1, 101):
+            if i % 3 == 0 && i % 5 == 0:
+                print("FizzBuzz")
+            elif i % 3 == 0:
+                print("Fizz")
+            elif i % 5 == 0:
+                print("Buzz")
+            else:
+                print(i)
